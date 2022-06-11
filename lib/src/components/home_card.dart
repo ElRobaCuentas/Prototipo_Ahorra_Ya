d8fcd6dd656f04ea066adb4491ef07e7bbd01768
GianMatty <matiasmauriciogiancarlo@gmail.com>
2022-06-11 17:04:39 -0500

    updated

--- lib/src/components/home_card.dart
+++ lib/src/components/home_card.dart
@@ -18,17 +18,65 @@ class HomeCard extends StatelessWidget {
               border: Border.all(color: Colors.black, width: 1),
               // color: Colors.red,
             ),
-            height: 50,
-            child: Text(
-              "hola mundo",
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              style: TextStyle(
-                color: Colors.red,
-                fontSize: 20.0,
-              ),
+            height: 36,
+            child: Row(
+              children: <Widget>[
+                Text(
+                  "hola mundo",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ],
             )),
-        Container(margin: EdgeInsets.all(16), child: Text("hola mundo")),
+        Container(
+            margin: EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(24),
+                        color: Colors.green),
+                    child: Center(
+                      child: Image(
+                        image: NetworkImage(
+                            "https://e39a9f00db6c5bc097f9-75bc5dce1d64f93372e7c97ed35869cb.ssl.cf1.rackcdn.com/40684201-2ska-C16-large.jpg"),
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                    // child: Center(
+                    //   child: Image(
+                    //       image: NetworkImage(
+                    //           "https://e39a9f00db6c5bc097f9-75bc5dce1d64f93372e7c97ed35869cb.ssl.cf1.rackcdn.com/40684201-2ska-C16-large.jpg")),
+                    // )
+                    ),
+                Container(
+                    width: 130,
+                    height: 65,
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: Center(
+                      child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: <Widget>[
+                            Icon(Icons.shopify, size: 36, color: Colors.green),
+                            Container(
+                                child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    children: <Widget>[
+                                  Text("0.50 centavo",
+                                      style: TextStyle(fontSize: 16)),
+                                  Text("de retorno",
+                                      style: TextStyle(fontSize: 16))
+                                ]))
+                          ]),
+                    ))
+              ],
+            )),
       ]),
     );
   }
